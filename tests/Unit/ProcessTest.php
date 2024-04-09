a9e28a0ff50122d3f3a2f4209eb129af1319ba33
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 09:59:55 -0500

    Update ProcessTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/ProcessTest.php
+++ tests/Unit/ProcessTest.php
@@ -11,4 +11,8 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(Process::class)]
 final class ProcessTest extends TestCase
 {
+    public function testExample(): void
+    {
+        self::assertTrue(true);
+    }
 }
