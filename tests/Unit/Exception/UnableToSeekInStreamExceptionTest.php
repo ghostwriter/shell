fe4924f7098bfbd221aa046339fbbeef7df7b7cf
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 09:59:55 -0500

    Update UnableToSeekInStreamExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/UnableToSeekInStreamExceptionTest.php
+++ tests/Unit/Exception/UnableToSeekInStreamExceptionTest.php
@@ -11,4 +11,8 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(UnableToSeekInStreamException::class)]
 final class UnableToSeekInStreamExceptionTest extends TestCase
 {
+    public function testExample(): void
+    {
+        self::assertTrue(true);
+    }
 }
