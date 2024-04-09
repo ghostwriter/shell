6f34b5d82a635782eacad3c44eeec05999ebc7b6
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 09:59:58 -0500

    Update FailedToWriteToStreamExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/FailedToWriteToStreamExceptionTest.php
+++ tests/Unit/Exception/FailedToWriteToStreamExceptionTest.php
@@ -11,4 +11,8 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(FailedToWriteToStreamException::class)]
 final class FailedToWriteToStreamExceptionTest extends TestCase
 {
+    public function testExample(): void
+    {
+        self::assertTrue(true);
+    }
 }
