89b875712f533b6702b63714f8e335c2e49fba99
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 10:00:02 -0500

    Update StderrInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Interface/Stdio/StderrInterface.php
+++ src/Interface/Stdio/StderrInterface.php
@@ -6,6 +6,4 @@ namespace Ghostwriter\Shell\Interface\Stdio;
 
 use Ghostwriter\Shell\Interface\DescriptorInterface;
 
-interface StderrInterface extends DescriptorInterface
-{
-}
+interface StderrInterface extends DescriptorInterface {}
