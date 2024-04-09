dac792842427fdc7cc94228a69d70fa167b7c471
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 09:59:58 -0500

    Update FailedToWriteToStdinExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/FailedToWriteToStdinExceptionTest.php
+++ tests/Unit/Exception/FailedToWriteToStdinExceptionTest.php
@@ -11,4 +11,8 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(FailedToWriteToStdinException::class)]
 final class FailedToWriteToStdinExceptionTest extends TestCase
 {
+    public function testExample(): void
+    {
+        self::assertTrue(true);
+    }
 }
