588a4f7483c93c17e65e0e710932b4ae5fe7b54f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 09:59:59 -0500

    Update FailedToReadFromPipeExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/FailedToReadFromPipeExceptionTest.php
+++ tests/Unit/Exception/FailedToReadFromPipeExceptionTest.php
@@ -11,4 +11,8 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(FailedToReadFromPipeException::class)]
 final class FailedToReadFromPipeExceptionTest extends TestCase
 {
+    public function testExample(): void
+    {
+        self::assertTrue(true);
+    }
 }
