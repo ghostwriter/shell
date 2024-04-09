53f84a7dc72baf1f12a1a67727b2de09a91a935e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 09:59:54 -0500

    Update StatusTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/StatusTest.php
+++ tests/Unit/StatusTest.php
@@ -11,4 +11,8 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(Status::class)]
 final class StatusTest extends TestCase
 {
+    public function testExample(): void
+    {
+        self::assertTrue(true);
+    }
 }
