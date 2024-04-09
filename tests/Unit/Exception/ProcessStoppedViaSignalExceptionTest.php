d08f9a879221873f7947e59941b85d295e6d6b77
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 09:59:56 -0500

    Update ProcessStoppedViaSignalExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/ProcessStoppedViaSignalExceptionTest.php
+++ tests/Unit/Exception/ProcessStoppedViaSignalExceptionTest.php
@@ -11,4 +11,8 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(ProcessStoppedViaSignalException::class)]
 final class ProcessStoppedViaSignalExceptionTest extends TestCase
 {
+    public function testExample(): void
+    {
+        self::assertTrue(true);
+    }
 }
