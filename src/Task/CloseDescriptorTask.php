7e419a58a7ea151c7fd0f2c6ad3e56819d7861fd
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-03 20:30:05 -0500

    Update CloseDescriptorTask.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Task/CloseDescriptorTask.php
+++ src/Task/CloseDescriptorTask.php
@@ -8,12 +8,14 @@ use Fiber;
 use Ghostwriter\Shell\Interface\DescriptorInterface;
 use Ghostwriter\Shell\Interface\TaskInterface;
 use Throwable;
+use Override;
 
 final class CloseDescriptorTask implements TaskInterface
 {
     /**
      * @throws Throwable
      */
+    #[Override]
     public function __invoke(DescriptorInterface $descriptor): void
     {
         do {
