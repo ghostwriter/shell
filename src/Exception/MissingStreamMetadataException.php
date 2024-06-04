cba10c696446bfece83cc0fbb9924d86c8a2b5d3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-03 20:30:08 -0500

    Add MissingStreamMetadataException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Exception/MissingStreamMetadataException.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Shell\Exception;
+
+use Ghostwriter\Shell\Interface\ExceptionInterface;
+use RuntimeException;
+
+final class MissingStreamMetadataException extends RuntimeException implements ExceptionInterface {}
