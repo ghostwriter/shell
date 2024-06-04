4c5fae108ea4361cb37d6ce2b35ff6e784b81ca3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-03 20:30:07 -0500

    Update ReadableInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Interface/Descriptor/ReadableInterface.php
+++ src/Interface/Descriptor/ReadableInterface.php
@@ -5,27 +5,19 @@ declare(strict_types=1);
 namespace Ghostwriter\Shell\Interface\Descriptor;
 
 use Ghostwriter\Shell\Interface\DescriptorInterface;
-use RuntimeException;
+use Throwable;
 
 interface ReadableInterface
 {
-    //    /**
-    //     * Returns whether the stream is readable.
-    //     */
-    //    public function isReadable(): bool;
-
     /**
      * Read data from the stream.
      *
      * @param int $length Read up to $length bytes from the object and return them.
-     *                    Fewer than $length bytes may be returned if underlying stream
-     *                    call returns fewer bytes.
+     *                    Fewer than $length bytes may be returned, if underlying stream call returns fewer bytes.
      *
-     * @throws RuntimeException if an error occurs
+     * @throws Throwable if an error occurs
      *
-     * @return string returns the data read from the stream, or an empty string
-     *                if no bytes are available
-     *
+     * @return string returns the data read from the stream, or an empty string if no bytes are available
      */
     public function read(int $length = DescriptorInterface::LENGTH): string;
 
