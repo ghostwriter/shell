f85909dece100f7dd5d9f144c0e363540863b546
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-03 20:30:07 -0500

    Update WritableInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Interface/Descriptor/WritableInterface.php
+++ src/Interface/Descriptor/WritableInterface.php
@@ -4,21 +4,16 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Shell\Interface\Descriptor;
 
-use Ghostwriter\Shell\Interface\RuntimeException;
+use Throwable;
 
 interface WritableInterface
 {
-    //    /**
-    //     * Returns whether the stream is writable.
-    //     */
-    //    public function isWritable(): bool;
-
     /**
      * Write data to the stream.
      *
      * @param string $bytes the string that is to be written
      *
-     * @throws RuntimeException on failure
+     * @throws Throwable on failure
      *
      * @return int returns the number of bytes written to the stream
      *
