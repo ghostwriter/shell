3c82f6785846a1592a1427a440a97cfe51942bf4
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-03 20:30:06 -0500

    Update Status.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Status.php
+++ src/Status.php
@@ -7,11 +7,16 @@ namespace Ghostwriter\Shell;
 use Ghostwriter\Shell\Exception\ProcessStoppedViaSignalException;
 use Ghostwriter\Shell\Exception\ProcessTerminatedViaSignalException;
 use Ghostwriter\Shell\Interface\StatusInterface;
+use Override;
 
 use function proc_get_status;
 
 final readonly class Status implements StatusInterface
 {
+    /**
+     * @throws ProcessStoppedViaSignalException
+     * @throws ProcessTerminatedViaSignalException
+     */
     public function __construct(
         public string $command,
         public int $exitCode,
@@ -31,41 +36,49 @@ final readonly class Status implements StatusInterface
         }
     }
 
+    #[Override]
     public function command(): string
     {
         return $this->command;
     }
 
+    #[Override]
     public function exitCode(): int
     {
         return $this->exitCode;
     }
 
+    #[Override]
     public function pid(): int
     {
         return $this->pid;
     }
 
+    #[Override]
     public function running(): bool
     {
         return $this->running;
     }
 
+    #[Override]
     public function signaled(): bool
     {
         return $this->signaled;
     }
 
+    #[Override]
     public function stopSignal(): int
     {
         return $this->stopSignal;
     }
 
+    #[Override]
     public function stopped(): bool
     {
         return $this->stopped;
     }
 
+    #[Override]
     public function terminateSignal(): int
     {
         return $this->terminateSignal;
@@ -73,6 +86,9 @@ final readonly class Status implements StatusInterface
 
     /**
      * @param resource $process
+     *
+     * @throws ProcessStoppedViaSignalException
+     * @throws ProcessTerminatedViaSignalException
      */
     public static function new(mixed $process): self
     {
