e401916e85ea51e8de61da8fb1e0f0c1b356ee3c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-03 20:30:05 -0500

    Update ReadDescriptorTask.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Task/ReadDescriptorTask.php
+++ src/Task/ReadDescriptorTask.php
@@ -8,9 +8,11 @@ use Fiber;
 use Ghostwriter\Shell\Exception\FailedToReadFromStreamException;
 use Ghostwriter\Shell\Interface\DescriptorInterface;
 use Ghostwriter\Shell\Interface\TaskInterface;
+use Override;
 
 final class ReadDescriptorTask implements TaskInterface
 {
+    #[Override]
     public function __invoke(DescriptorInterface $descriptor): void
     {
         do {
