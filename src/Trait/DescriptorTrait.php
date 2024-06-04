72c17fe6b7c03c22b1952872013979b4e3fd30b3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-03 20:30:05 -0500

    Update DescriptorTrait.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Trait/DescriptorTrait.php
+++ src/Trait/DescriptorTrait.php
@@ -4,19 +4,21 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Shell\Trait;
 
+use Ghostwriter\Shell\Exception\EmptyStringException;
 use Ghostwriter\Shell\Exception\FailedToClosePipeException;
 use Ghostwriter\Shell\Exception\FailedToReadFromStreamException;
 use Ghostwriter\Shell\Exception\FailedToSetStreamBlockingException;
 use Ghostwriter\Shell\Exception\FailedToWriteToStreamException;
-use Ghostwriter\Shell\Exception\InvalidStreamResourceException;
+use Ghostwriter\Shell\Exception\MissingStreamMetadataException;
 use Ghostwriter\Shell\Exception\StreamIsNotReadableException;
-use Ghostwriter\Shell\Exception\StreamIsNotSeekableException;
+use Ghostwriter\Shell\Exception\StreamIsNotResourceException;
 use Ghostwriter\Shell\Exception\StreamIsNotWritableException;
-use Ghostwriter\Shell\Exception\UnableToSeekInStreamException;
-use Ghostwriter\Shell\Exception\UnableToTellStreamPositionException;
+use Ghostwriter\Shell\Interface\BufferInterface;
 use Ghostwriter\Shell\Interface\DescriptorInterface;
-use Throwable;
+use Ghostwriter\Shell\StringBuffer;
+use Override;
 
+use function array_key_exists;
 use function fclose;
 use function feof;
 use function fgets;
@@ -24,9 +26,12 @@ use function fread;
 use function fstat;
 use function fwrite;
 use function is_resource;
+use function is_writable;
+use function str_contains;
 use function stream_get_contents;
 use function stream_get_meta_data;
 use function stream_set_blocking;
+use function trim;
 
 trait DescriptorTrait
 {
@@ -34,31 +39,27 @@ trait DescriptorTrait
      * @param null|resource $stream
      *
      * @throws FailedToSetStreamBlockingException
+     * @throws StreamIsNotResourceException
      */
     public function __construct(
         private mixed $stream,
-        private string $buffer = '',
+        private readonly BufferInterface $buffer,
     ) {
-        $this->assertIsResource();
-
-        if (! stream_set_blocking($this->stream, false)) {
-            throw new FailedToSetStreamBlockingException();
-        }
-    }
-
-    public function __destruct()
-    {
-        $this->close();
-    }
-
-    public function __toString(): string
-    {
-        return $this->toString();
+        $this->assertNonBlockingResource();
     }
 
     /**
      * @throws FailedToClosePipeException
      */
+    public function __destruct()
+    {
+        $this->close();
+    }
+
+    /**
+     * @throws FailedToClosePipeException
+     */
+    #[Override]
     final public function close(): void
     {
         if ($this->stream === null) {
@@ -76,7 +77,11 @@ trait DescriptorTrait
         }
     }
 
-    public function detach(): mixed
+    /**
+     * @return null|resource
+     */
+    #[Override]
+    final public function detach(): mixed
     {
         $resource = $this->stream;
 
@@ -85,19 +90,45 @@ trait DescriptorTrait
         return $resource;
     }
 
-    public function getMetadata(?string $key = null): mixed
+    /**
+     *
+     * @throws StreamIsNotResourceException
+     * @throws MissingStreamMetadataException
+     *
+     * @return array{
+     *      timed_out: bool,
+     *      blocked: bool,
+     *      eof: bool,
+     *      unread_bytes: int,
+     *      stream_type: string,
+     *      wrapper_type: string,
+     *      wrapper_data: mixed,
+     *      mode: string,
+     *      seekable: bool,
+     *      uri: string,
+     *      crypto: array,
+     *      mediatype: string
+     *   }|bool|mixed|null
+     *
+     */
+    final public function getMetadata(?string $key = null): mixed
     {
         $this->assertIsResource();
 
         $meta = stream_get_meta_data($this->stream);
+
         if ($key === null) {
             return $meta;
         }
 
-        return $meta[$key] ?? null;
+        if (! array_key_exists($key, $meta)) {
+            throw new MissingStreamMetadataException();
+        }
+
+        return $meta[$key];
     }
 
-    public function getSize(): ?int
+    final public function getSize(): ?int
     {
         if (! is_resource($this->stream)) {
             return null;
@@ -111,6 +142,7 @@ trait DescriptorTrait
         return $stats['size'] ?? null;
     }
 
+    #[Override]
     final public function isRunning(): bool
     {
         if (! is_resource($this->stream)) {
@@ -120,49 +152,32 @@ trait DescriptorTrait
         return ! feof($this->stream);
     }
 
-    public function read(int $length = DescriptorInterface::LENGTH): string
+    /**
+     * @throws FailedToReadFromStreamException
+     * @throws StreamIsNotResourceException
+     * @throws StreamIsNotReadableException
+     */
+    #[Override]
+    final public function read(int $length = DescriptorInterface::LENGTH): string
     {
-        $this->assertIsResource();
-
-        //        if ($this->readable === false) {
-        //            throw new StreamIsNotReadableException();
-        //        }
+        $this->assertIsReadable();
 
         $bytesRead = fread($this->stream, $length);
         if ($bytesRead === false) {
             throw new FailedToReadFromStreamException();
         }
 
-        $this->buffer.= $bytesRead;
+        $this->buffer->append($bytesRead);
 
         return $bytesRead;
     }
 
-    //    /**
-    //     * Returns true if the stream is readable.
-    //     */
-    //    public function isReadable(): bool
-    //    {
-    //        return $this->readable && is_resource($this->stream);
-    //    }
-    //
-    //    /**
-    //     * Returns true if the stream is seekable.
-    //     */
-    //    public function isSeekable(): bool
-    //    {
-    //        return $this->seekable && is_resource($this->stream);
-    //    }
-    //
-    //    /**
-    //     * Returns true if the stream is writable.
-    //     */
-    //    public function isWritable(): bool
-    //    {
-    //        return $this->writable && is_resource($this->stream);
-    //    }
-
-    public function readLine(): string
+    /**
+     * @throws FailedToReadFromStreamException
+     * @throws StreamIsNotResourceException
+     */
+    #[Override]
+    final public function readLine(): string
     {
         $this->assertIsResource();
 
@@ -171,97 +186,47 @@ trait DescriptorTrait
             throw new FailedToReadFromStreamException();
         }
 
-        $this->buffer.= $bytesRead;
+        $this->buffer->append($bytesRead);
 
         return $bytesRead;
     }
 
+    /**
+     * @throws FailedToReadFromStreamException
+     * @throws StreamIsNotResourceException
+     * @throws StreamIsNotReadableException
+     */
+    #[Override]
     final public function toString(): string
     {
         if ($this->stream === null) {
-            return $this->buffer;
+            return $this->buffer->toString();
         }
 
-        try {
-            //            if ($this->seekable) {
-            //                $this->rewind();
-            //            }
+        $this->assertIsReadable();
 
-            $this->assertIsResource();
-
-            //            if ($this->readable === false) {
-            //                throw new StreamIsNotReadableException();
-            //            }
-
-            $contents = stream_get_contents($this->stream);
-            if ($contents === false) {
-                throw new FailedToReadFromStreamException();
-            }
-
-            return $this->buffer .= $contents;
-        } catch (Throwable) {
-            return $this->buffer;
+        $contents = stream_get_contents($this->stream);
+        if ($contents === false) {
+            throw new FailedToReadFromStreamException();
         }
+
+        return $this->buffer->toString() . $contents;
     }
 
-    //    public function rewind(): void
-    //    {
-    //        $this->seek(0);
-    //    }
-    //
-    //    public function seek(int $offset, int $whence = SEEK_SET): void
-    //    {
-    //        $this->assertIsResource();
-    //
-    //        if (fseek($this->stream, $offset, $whence) === -1) {
-    //            throw new StreamIsNotSeekableException();
-    //        }
-    //
-    //        if ($this->stream === null) {
-    //            throw new InvalidStreamResourceException();
-    //        }
-    //
-    ////        if ($this->seekable === false) {
-    ////            throw new StreamIsNotSeekableException();
-    ////        }
-    //
-    //        if (fseek($this->stream, $offset, $whence) === -1) {
-    //            throw new UnableToSeekInStreamException();
-    //        }
-    //    }
-
-    //    public function tell(): int
-    //    {
-    //        $this->assertIsResource();
-    //
-    //        $offset = ftell($this->stream);
-    //        if ($offset === false) {
-    //            throw new UnableToTellStreamPositionException();
-    //        }
-    //
-    //        return $offset;
-    //    }
-
-    //    public function toString(): string
-    //    {
-    //        if ($this->stream === null) {
-    //            return $this->buffer;
-    //        }
-    //
-    //        try {
-    //            return $this->asString();
-    //        } catch (Throwable) {
-    //            return $this->buffer;
-    //        }
-    //    }
-
-    public function write(string $bytes): int
+    /**
+     * @throws EmptyStringException
+     * @throws FailedToWriteToStreamException
+     * @throws StreamIsNotResourceException
+     * @throws StreamIsNotWritableException
+     *
+     * @return positive-int
+     */
+    #[Override]
+    final public function write(string $bytes): int
     {
-        $this->assertIsResource();
+        $this->assertNotEmpty($bytes);
 
-        //        if ($this->writable === false) {
-        //            throw new StreamIsNotWritableException();
-        //        }
+        $this->assertIsWritable();
 
         $bytesWritten = fwrite($this->stream, $bytes);
         if ($bytesWritten === false) {
@@ -272,29 +237,100 @@ trait DescriptorTrait
     }
 
     /**
+     * @psalm-assert-if-true non-empty-string $bytes
+     *
+     * @throws EmptyStringException
+     */
+    private function assertNotEmpty(string $bytes): void
+    {
+        if (trim($bytes) === '') {
+            throw new EmptyStringException();
+        }
+    }
+
+    /**
      * @psalm-assert resource $this->stream
      *
      * @psalm-this-out resource $this->resource
      *
-     * @throws InvalidStreamResourceException
+     * @throws StreamIsNotResourceException
      */
     private function assertIsResource(): void
     {
         if (! is_resource($this->stream)) {
-            throw new InvalidStreamResourceException();
+            throw new StreamIsNotResourceException();
+        }
+    }
+
+    /**
+     * @psalm-assert resource $this->stream
+     *
+     * @psalm-this-out resource $this->resource
+     *
+     * @throws FailedToSetStreamBlockingException
+     * @throws StreamIsNotResourceException
+     */
+    private function assertNonBlockingResource(): void
+    {
+        $this->assertIsResource();
+
+        if (! stream_set_blocking($this->stream, false)) {
+            throw new FailedToSetStreamBlockingException();
+        }
+    }
+
+    /**
+     * @psalm-assert resource $this->stream
+     *
+     * @throws StreamIsNotReadableException
+     * @throws StreamIsNotResourceException
+     */
+    private function assertIsReadable(): void
+    {
+        $this->assertIsResource();
+
+        $metaData = stream_get_meta_data($this->stream);
+
+        $mode = $metaData['mode'] ?? '';
+
+        if (! str_contains($mode, 'r')) {
+            return;
+        }
+
+        if (! str_contains($mode, '+')) {
+            return;
+        }
+
+        throw new StreamIsNotReadableException();
+    }
+
+    /**
+     * @psalm-assert resource $this->stream
+     *
+     * @psalm-this-out resource $this->resource
+     *
+     * @throws StreamIsNotResourceException
+     * @throws StreamIsNotWritableException
+     */
+    private function assertIsWritable(): void
+    {
+        $this->assertIsResource();
+
+        if (! is_writable($this->stream)) {
+            throw new StreamIsNotWritableException();
         }
     }
 
     /**
      * @param resource $stream
      *
-     * @throws InvalidStreamResourceException
+     * @throws StreamIsNotResourceException
      * @throws FailedToSetStreamBlockingException
      */
     public static function new(mixed $stream): self
     {
         if (! is_resource($stream)) {
-            throw new InvalidStreamResourceException();
+            throw new StreamIsNotResourceException();
         }
 
         //        $meta = stream_get_meta_data($resourceOrStream);
@@ -302,6 +338,6 @@ trait DescriptorTrait
         //        $read = str_contains($mode, 'r');
         //        $plus = str_contains($mode, '+');
 
-        return new self($stream);
+        return new self($stream, StringBuffer::new());
     }
 }
