e0a1f95a52689d57887c985448ede7dbfaa34c03
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-03 20:30:03 -0500

    Update CloseDescriptorTaskTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Task/CloseDescriptorTaskTest.php
+++ tests/Unit/Task/CloseDescriptorTaskTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\ShellTests\Unit\Task;
+namespace Tests\Unit\Task;
 
 use Ghostwriter\Shell\Task\CloseDescriptorTask;
 use PHPUnit\Framework\Attributes\CoversClass;
