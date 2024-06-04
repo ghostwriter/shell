15ad28e8eb61ff835f7167f8acabe600f3c5cdaf
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-03 20:30:07 -0500

    Add StringBuffer.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/StringBuffer.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Shell;
+
+use Ghostwriter\Shell\Interface\BufferInterface;
+use Override;
+
+final class StringBuffer implements BufferInterface
+{
+    public function __construct(
+        private string $buffer
+    ) {}
+
+    public static function new(string $buffer = ''): self
+    {
+        return new self($buffer);
+    }
+
+    #[Override]
+    public function append(string $bytes): void
+    {
+        $this->buffer .= $bytes;
+    }
+
+    #[Override]
+    public function toString(): string
+    {
+        return $this->buffer;
+    }
+}
