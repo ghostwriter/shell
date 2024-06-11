7301e48d14a6a2d8845d13456035df4f1a4659f5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-11 11:34:53 -0500

    Update UnableToGetStreamContentsException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/UnableToGetStreamContentsException.php
+++ src/Exception/UnableToGetStreamContentsException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Shell\Exception;
 use Ghostwriter\Shell\Interface\ExceptionInterface;
 use RuntimeException;
 
-final class UnableToGetStreamContentsException extends RuntimeException implements ExceptionInterface {}
+final class UnableToGetStreamContentsException extends RuntimeException implements ExceptionInterface
+{
+}
