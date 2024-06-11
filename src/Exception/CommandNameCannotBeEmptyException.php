ff2d96d82a375bbf8ac7abfdcb1cbc4feca3e863
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-11 11:34:55 -0500

    Update CommandNameCannotBeEmptyException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/CommandNameCannotBeEmptyException.php
+++ src/Exception/CommandNameCannotBeEmptyException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Shell\Exception;
 use Ghostwriter\Shell\Interface\ExceptionInterface;
 use RuntimeException;
 
-final class CommandNameCannotBeEmptyException extends RuntimeException implements ExceptionInterface {}
+final class CommandNameCannotBeEmptyException extends RuntimeException implements ExceptionInterface
+{
+}
