d729cea7128c68a1e0cc900ebdc09473c14fb32f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-11 11:34:53 -0500

    Update StreamIsNotReadableException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/StreamIsNotReadableException.php
+++ src/Exception/StreamIsNotReadableException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Shell\Exception;
 use Ghostwriter\Shell\Interface\ExceptionInterface;
 use RuntimeException;
 
-final class StreamIsNotReadableException extends RuntimeException implements ExceptionInterface {}
+final class StreamIsNotReadableException extends RuntimeException implements ExceptionInterface
+{
+}
