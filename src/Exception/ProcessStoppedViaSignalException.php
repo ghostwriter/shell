007843424a9279477255f2151b4c52895e434cc6
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-11 11:34:53 -0500

    Update ProcessStoppedViaSignalException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/ProcessStoppedViaSignalException.php
+++ src/Exception/ProcessStoppedViaSignalException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Shell\Exception;
 use Ghostwriter\Shell\Interface\ExceptionInterface;
 use RuntimeException;
 
-final class ProcessStoppedViaSignalException extends RuntimeException implements ExceptionInterface {}
+final class ProcessStoppedViaSignalException extends RuntimeException implements ExceptionInterface
+{
+}
