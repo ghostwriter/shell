e88507999950e3df9f5a2a2fd5b2b205197fad6e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-11 11:34:54 -0500

    Update FailedToSetStreamBlockingException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/FailedToSetStreamBlockingException.php
+++ src/Exception/FailedToSetStreamBlockingException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Shell\Exception;
 use Ghostwriter\Shell\Interface\ExceptionInterface;
 use RuntimeException;
 
-final class FailedToSetStreamBlockingException extends RuntimeException implements ExceptionInterface {}
+final class FailedToSetStreamBlockingException extends RuntimeException implements ExceptionInterface
+{
+}
