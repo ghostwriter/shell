9e7709a50c972d1572fbbab091b6cff94f622aa5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-11 11:34:53 -0500

    Update ProcOpenFunctionNotAvailableException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/ProcOpenFunctionNotAvailableException.php
+++ src/Exception/ProcOpenFunctionNotAvailableException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Shell\Exception;
 use Ghostwriter\Shell\Interface\ExceptionInterface;
 use RuntimeException;
 
-final class ProcOpenFunctionNotAvailableException extends RuntimeException implements ExceptionInterface {}
+final class ProcOpenFunctionNotAvailableException extends RuntimeException implements ExceptionInterface
+{
+}
