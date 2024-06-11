072821bbcc6fcf34495697cde9879bb8df618622
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-11 11:34:54 -0500

    Update FailedToOpenProcessException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/FailedToOpenProcessException.php
+++ src/Exception/FailedToOpenProcessException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Shell\Exception;
 use Ghostwriter\Shell\Interface\ExceptionInterface;
 use RuntimeException;
 
-final class FailedToOpenProcessException extends RuntimeException implements ExceptionInterface {}
+final class FailedToOpenProcessException extends RuntimeException implements ExceptionInterface
+{
+}
