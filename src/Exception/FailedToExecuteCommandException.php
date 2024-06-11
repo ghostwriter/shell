d27440e7c11dc4f260c34b5491de34d0c8745f7a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-11 11:34:55 -0500

    Update FailedToExecuteCommandException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/FailedToExecuteCommandException.php
+++ src/Exception/FailedToExecuteCommandException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Shell\Exception;
 use Ghostwriter\Shell\Interface\ExceptionInterface;
 use RuntimeException;
 
-final class FailedToExecuteCommandException extends RuntimeException implements ExceptionInterface {}
+final class FailedToExecuteCommandException extends RuntimeException implements ExceptionInterface
+{
+}
