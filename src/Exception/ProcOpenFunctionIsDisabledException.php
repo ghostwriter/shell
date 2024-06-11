52e7f2da797bce2ec2b94242dc772be657535b4c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-11 11:34:53 -0500

    Update ProcOpenFunctionIsDisabledException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/ProcOpenFunctionIsDisabledException.php
+++ src/Exception/ProcOpenFunctionIsDisabledException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Shell\Exception;
 use Ghostwriter\Shell\Interface\ExceptionInterface;
 use RuntimeException;
 
-final class ProcOpenFunctionIsDisabledException extends RuntimeException implements ExceptionInterface {}
+final class ProcOpenFunctionIsDisabledException extends RuntimeException implements ExceptionInterface
+{
+}
