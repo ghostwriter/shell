e832f234a6a15c058cc65ba941b84415efe7e2f9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-11 11:34:54 -0500

    Update FailedToReadFromPipeException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/FailedToReadFromPipeException.php
+++ src/Exception/FailedToReadFromPipeException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Shell\Exception;
 use Ghostwriter\Shell\Interface\ExceptionInterface;
 use RuntimeException;
 
-final class FailedToReadFromPipeException extends RuntimeException implements ExceptionInterface {}
+final class FailedToReadFromPipeException extends RuntimeException implements ExceptionInterface
+{
+}
