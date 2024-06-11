b842917b165b2d31be35b17a60e75b2685c1ba2e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-11 11:34:54 -0500

    Update InvalidLengthException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/InvalidLengthException.php
+++ src/Exception/InvalidLengthException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Shell\Exception;
 use Ghostwriter\Shell\Interface\ExceptionInterface;
 use RuntimeException;
 
-final class InvalidLengthException extends RuntimeException implements ExceptionInterface {}
+final class InvalidLengthException extends RuntimeException implements ExceptionInterface
+{
+}
