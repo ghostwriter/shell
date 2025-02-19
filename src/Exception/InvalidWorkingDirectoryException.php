25ea2b93a1e45da46607dcec4b08899014ea0a75
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-18 22:28:46 -0600

    Update InvalidWorkingDirectoryException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/InvalidWorkingDirectoryException.php
+++ src/Exception/InvalidWorkingDirectoryException.php
@@ -7,6 +7,4 @@ namespace Ghostwriter\Shell\Exception;
 use Ghostwriter\Shell\Interface\ExceptionInterface;
 use RuntimeException;
 
-final class InvalidWorkingDirectoryException extends RuntimeException implements ExceptionInterface
-{
-}
+final class InvalidWorkingDirectoryException extends RuntimeException implements ExceptionInterface {}
