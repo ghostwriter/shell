993f4b43f043b514e64780b9ded6d9f2be1ca9d2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-18 22:28:47 -0600

    Update NullPointerException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/NullPointerException.php
+++ src/Exception/NullPointerException.php
@@ -7,6 +7,4 @@ namespace Ghostwriter\Shell\Exception;
 use Ghostwriter\Shell\Interface\ExceptionInterface;
 use RuntimeException;
 
-final class NullPointerException extends RuntimeException implements ExceptionInterface
-{
-}
+final class NullPointerException extends RuntimeException implements ExceptionInterface {}
