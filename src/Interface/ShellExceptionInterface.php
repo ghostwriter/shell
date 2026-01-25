2735f90216bf4297f65a912a4e0f5f70e13da42b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2026-01-25 10:03:32 -0600

    Introduce ShellExceptionInterface and update ExceptionInterface
    Added ShellExceptionInterface extending Throwable and updated ExceptionInterface to extend ShellExceptionInterface instead of Throwable directly.

--- src/Interface/ShellExceptionInterface.php
+++ src/Interface/ShellExceptionInterface.php
@@ -4,4 +4,6 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Shell\Interface;
 
-interface ExceptionInterface extends ShellExceptionInterface {}
+use Throwable;
+
+interface ShellExceptionInterface extends Throwable {}
